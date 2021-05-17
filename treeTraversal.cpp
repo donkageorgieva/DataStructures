c6f618c0fd5480207e0abd6fe08bb737b2a43ebe
Donka Georgieva <65857282+donkageorgieva@users.noreply.github.com>
2021-05-17 13:00:31 +0300

    Add files via upload

--- treeTraversal.cpp
+++ treeTraversal.cpp
@@ -8,26 +8,31 @@ public:
     int data;
     Node *left;
     Node *right;
-
-    void breadthFirstTraversal(Node *root)
-    {
-        if (root == NULL)
-            return;
-
-        queue<Node *> Q;
-        Q.push(root);
-        while (!Q.empty())
-        {
-            Node *current = Q.front();
-            cout << current->data << endl;
-            if (current->left != NULL)
-                Q.push(current->left);
-            if (current->right != NULL)
-                Q.push(current->right);
-            Q.pop();
-        }
-    }
 };
+void breadthFirstTraversal(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    queue<Node *> Q;
+    Q.push(root);
+    while (!Q.empty())
+    {
+        Node *current = Q.front();
+        cout << current->data << endl;
+        if (current->left != NULL)
+        {
+            Q.push(current->left);
+        }
+
+        if (current->right != NULL)
+        {
+            Q.push(current->right);
+        }
+
+        Q.pop();
+    }
+}
 Node *insertANode(Node *root, int value)
 {
     Node *temp = new Node();
@@ -53,8 +58,10 @@ void depthFirstPreOrder(Node *root)
 {
 
     if (root == NULL)
+    {
         return;
-    cout << root << endl;
+    }
+    cout << root->data << ' ';
 
     depthFirstPreOrder(root->left);
 
@@ -74,20 +81,63 @@ bool isBinarySearchTree(Node *root, int minValue, int maxValue)
     else
         return false;
 }
+Node *minNode(Node *root)
+{
+    while (root != NULL)
+    {
+        root = root->left;
+    }
+    return root;
+}
+Node *deleteANode(Node *root, int value)
+{
+    if (root == NULL)
+        return root;
+
+    else if (root->data > value)
+        root->left = deleteANode(root->left, value);
+    else if (root->data < value)
+        root->right = deleteANode(root->right, value);
+    else
+    {
+        if (root->left == NULL && root->right == NULL)
+        {
+            delete root;
+            root = NULL;
+        }
+        else if (root->left == NULL)
+        {
+            Node *temp = root;
+            root = root->right;
+            delete temp;
+        }
+        else if (root->right == NULL)
+        {
+            Node *temp = root;
+            root = root->left;
+            delete temp;
+        }
+        else
+        {
+            Node *temp = minNode(root->right);
+            root->data = temp->data;
+            root->right = deleteANode(root->right, temp->data);
+        }
+    }
+    return root;
+}
 int main()
 {
     Node *root = NULL;
-    root = insertANode(root, 600);
-    root = insertANode(root, 500);
-    root = insertANode(root, 600);
-    root = insertANode(root, 20);
-    root = insertANode(root, 8);
-    root = insertANode(root, 60);
-    root = insertANode(root, 10);
-    root = insertANode(root, 15);
+    root = insertANode(root, 50);
     root = insertANode(root, 25);
-    // depthFirstPreOrder(root);
+    root = insertANode(root, 15);
 
     // root->breadthFirstTraversal(root);
-    cout << isBinarySearchTree(root, INT_MAX, INT_MIN);
+
+    depthFirstPreOrder(root);
+    deleteANode(root, 15);
+    depthFirstPreOrder(root);
+    root = insertANode(root, 10);
+    depthFirstPreOrder(root);
 }
